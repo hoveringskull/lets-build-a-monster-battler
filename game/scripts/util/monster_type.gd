c65e448475b21c4c859626935cedf467b2e94394
Joshua Grant <jc.grant22@gmail.com>
2025-07-30 22:20:07 -0400

    2-3 types and weaknesses

--- game/scripts/util/monster_type.gd
+++ game/scripts/util/monster_type.gd
@@ -36,3 +36,12 @@ const EFFECTIVENESS_BY_TYPE: Dictionary[Type, Dictionary] = {
 		Type.WATER: Effectiveness.STRONG
 	},
 }
+
+static func get_type_effectiveness(source_type: Type, target_type: Type):
+	if !EFFECTIVENESS_BY_TYPE.has(source_type) or !EFFECTIVENESS_BY_TYPE[source_type].has(target_type):
+		return Effectiveness.NEUTRAL
+	return EFFECTIVENESS_BY_TYPE[source_type][target_type]
+
+static func get_type_advantage_coefficient(source_type: Type, target_type: Type):
+	var effectiveness = get_type_effectiveness(source_type, target_type)
+	return DAMAGE_MODIFIER_BY_EFFECT[effectiveness]
